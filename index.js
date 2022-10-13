43f7309f9c309b03e7ddfc40fe8b5ca60a65e22d
WebsiteBuilder1234 <113625382+WebsiteBuilder1234@users.noreply.github.com>
2022-10-13 10:23:20 -0400

    new changes

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("Someone opened say 'HI'");
-alert("Hi");
+//Hi, it looks like you are looking at my source code, do you like it?
+console.log("You Found My Easter Egg, Cool!");
+var Hi = "Hi";
+console.clear;
+console.log(Hi);
+var Welcome = "Welcome";
+alert(Welcome);
---
+++ index.ts
@@ -0,0 +1,7 @@
+//Hi, it looks like you are looking at my source code, do you like it?
+console.log("You Found My Easter Egg, Cool!")
+var Hi = "Hi"
+console.clear
+console.log(Hi)
+var Welcome = "Welcome"
+alert(Welcome)
